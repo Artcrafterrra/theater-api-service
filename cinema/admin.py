be6c3f1a17935e9f8d54e989a57aacf66fcfe414
Artcrafterrra <if.artcraft@gmail.com>
2025-09-16 16:28:24 +0300

    Register cinema models with the admin interface and configure model-specific admin options.

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from cinema.models import Actor, Genre, TheatreHall, Play, Performance, Ticket, Reservation
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name")
+    search_fields = ("first_name", "last_name")
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(TheatreHall)
+class TheatreHallAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "rows", "seats_in_row", "capacity")
+
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    search_fields = ("title",)
+    filter_horizontal = ("actors", "genres")
+
+
+@admin.register(Performance)
+class PerformanceAdmin(admin.ModelAdmin):
+    list_display = ("id", "play", "theatre_hall", "show_time")
+    list_filter = ("theatre_hall", "play")
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created_at")
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "performance", "row", "seat", "reservation")
+    list_filter = ("performance", "reservation")
+    search_fields = ("performance__play__title",)
+
